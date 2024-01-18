5673623e4dd71941dadc04938156faaf8f8862fd
MaksGorobets <gorobets542@gmail.com>
2024-01-18 19:33:30 +0200

    Initial Commit

---
+++ CustomFileFormatTest/CustomFileFormatTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFileFormatTestApp.swift
+//  CustomFileFormatTest
+//
+//  Created by Maks Winters on 18.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomFileFormatTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
